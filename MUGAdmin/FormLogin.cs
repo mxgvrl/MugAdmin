2ac0ee4350faa6208bb29c33a6216b4ef1b003d5
mxgvrl <mxgvrl@gmail.com>
2021-01-09 17:45:15 +0300

    Manage tables

---
+++ MUGAdmin/FormLogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MUGAdmin
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormMain formMain = new FormMain();
+            formMain.Show();
+        }
+    }
+}
